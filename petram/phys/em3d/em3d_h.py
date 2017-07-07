6ae9e11e2e830bd2e7da23f5605097e59993cd0e
Shiraiwa <shiraiwa@psfc.mit.edu>
2017-07-07 16:43:19 -0400

    update H

--- petram/phys/em3d/em3d_h.py
+++ petram/phys/em3d/em3d_h.py
@@ -28,6 +28,8 @@ if use_parallel:
 else:
    import mfem.ser as mfem
    
+name_suffix = ['_x', '_y', '_z']
+
 class Ht(VectorPhysCoefficient):
    def __init__(self, *args, **kwargs):
        omega = kwargs.pop('omega', 1.0)
@@ -59,7 +61,52 @@ class EM3D_H(Bdry, Phys):
         v['H_m'] = '[0.0, 0.0, 0.0]'        
         v['use_m_H'] = False                
         return v
-    
+     
+    def panel1_param(self):
+        return [self.make_matrix_panel('H', name_suffix, 3, 1)]
+        
+    def form_name(self, v):
+        if v: return 'Array Form'
+        else: return 'Elemental Form'
+
+    def get_panel1_value(self):
+        a = [self.form_name(self.use_m_H), 
+             [[str(getattr(self, 'H'+n)) for n in name_suffix]],
+             [str(self.H_m)]]
+        return [a, ]
+
+    def preprocess_params(self, engine):
+        dprint1('Preprocess H')
+        k = 0
+        for n in name_suffix:
+            setattr(self, 'H'+n, str(getattr(self, 'H' + n)))
+            k = k + 1
+
+    def import_panel1_value(self, v):
+        self.use_m_H = (str(v[0][0]) == 'Array Form')
+        for k, n in enumerate(name_suffix):
+            setattr(self, 'H'+n, str(v[0][1][0][k]))
+        self.H_m = str(v[0][2][0])
+
+    def _make_f_name(self):
+        basename = 'H'
+        if getattr(self, 'use_m_'+basename):
+            names = ['_m']
+            eval_expr = self.eval_phys_array_expr
+        else:
+            names = ['_x', '_y', '_z',]
+            eval_expr = self.eval_phys_expr
+              
+        f_name = []
+        for n in names:
+           var, f_name0 = eval_expr(getattr(self, basename+n), basename + n)
+           if f_name0 is None:
+               f_name.append(var)
+           else:
+               f_name.append(f_name0)
+        return f_name
+
+    ''' 
     def panel1_param(self):
         names = ['_x', '_y', '_z']
         
@@ -122,7 +169,7 @@ class EM3D_H(Bdry, Phys):
            else:
                f_name.append(f_name0)
         return f_name
-
+    '''
         
     def has_lf_contribution(self, kfes = 0):
         if kfes != 0: return False
