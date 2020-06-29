fixed coax normalization

petram/phys/em3d/em3d_port.py
@@ -208,7 +208,7 @@ class C_jwHt_TEM(mfem.VectorPyCoefficient):
    coax port TEM
 '''
 def coax_norm(a, b, mur, eps):
-    return np.sqrt(1/np.pi/np.sqrt(epsilon0*eps/mu0/mur)/np.log(b/a))
+    return np.sqrt(2/np.pi/np.sqrt(epsilon0*eps/mu0/mur)/np.log(b/a))
     
 class C_Et_CoaxTEM(mfem.VectorPyCoefficient):
    def __init__(self, sdim, bdry, real = True, eps=1.0, mur=1.0):
@@ -245,7 +245,7 @@ class C_jwHt_CoaxTEM(mfem.VectorPyCoefficient):
        self.ctr = bdry.ctr
        self.phase = phase  # phase !=0 for incoming wave
        #self.AA = omega*np.sqrt(epsilon0*eps/mu0/mur)
-       self.AA = coax_norm(self.a, self.b, mur, eps)*omega*np.sqrt(epsilon0*eps/mu0/mur)
+       self.AA = coax_norm(self.a, self.b, mur, eps)*omega*np.sqrt(epsilon0*eps/mu0/mur)*amp
        
    def EvalValue(self, x):
        r = (x - self.ctr)