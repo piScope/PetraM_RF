7c9506507ff264eb50476147804c20066c12e437
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-11-29 15:12:42 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='PetraM_RF',
-    version='1.0.5',
+    version='1.0.6',
 
     description='PetraM RF package',
     long_description=long_description,
