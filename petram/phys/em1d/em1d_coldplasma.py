32a711109ea0037a576da8c2b77864a9a8a1f050
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-06-07 18:25:04 -0400

    1D cold plasma may be working? need to fix plotting

--- petram/phys/em1d/em1d_coldplasma.py
+++ petram/phys/em1d/em1d_coldplasma.py
@@ -7,7 +7,6 @@ from petram.mfem_config import use_parallel, get_numba_debug
 
 from petram.phys.vtable import VtableElement, Vtable
 from petram.phys.phys_const import mu0, epsilon0
-from petram.phys.coefficient import SCoeff, VCoeff, MCoeff
 from petram.phys.numba_coefficient import NumbaCoefficient
 
 from petram.phys.phys_model import MatrixPhysCoefficient, PhysCoefficient, PhysConstant, PhysMatrixConstant
@@ -51,16 +50,16 @@ data = (('B', VtableElement('bext', type='array',
                                    default="1, 1",
                                    no_func=True,
                                    tip="ion charges normalized by q(=1.60217662e-19 [C])")),
-         ('ky', VtableElement('ky', type='int',
-                                     guilabel = 'ky',
-                                     default = 0. ,
-                                     no_func = True, 
-                                     tip = "wave number in the y direction" )),
-         ('kz', VtableElement('kz', type='int',
-                                     guilabel = 'kz',
-                                     default = 0.0,
-                                     no_func = True,                               
-                                     tip = "wave number in the z direction" )),)
+        ('ky', VtableElement('ky', type='int',
+                             guilabel='ky',
+                             default=0.,
+                             no_func=True,
+                             tip="wave number in the y direction")),
+        ('kz', VtableElement('kz', type='int',
+                             guilabel='kz',
+                             default=0.0,
+                             no_func=True,
+                             tip="wave number in the z direction")),)
 
 
 def domain_constraints():
@@ -72,20 +71,31 @@ class EM1D_ColdPlasma(EM1D_Vac):
     vt = Vtable(data)
     #nlterms = ['epsilonr']
 
+    def __init__(self, **kargs):
+        super(EM1D_ColdPlasma, self).__init__(**kargs)
+        self._jited_coeff = None
+
     def get_possible_child(self):
         return []
-    
-    def has_bf_contribution(self, kfes):
-        if kfes == 0:
-            return True
-        else:
-            return False
 
-    def get_coeffs(self, real=True, return_stix=False):
+    @property
+    def jited_coeff(self):
+        return self._jited_coeff
+
+    def compile_coeffs(self, n_matrix):
+        self._jited_coeff = self.get_coeffs()
+
+    def get_coeffs(self, real=True):
         freq, omega = self.get_root_phys().get_freq_omega()
         B, dens_e, t_e, dens_i, masses, charges, ky, kz = self.vt.make_value_or_expression(
             self)
+        ind_vars = self.get_root_phys().ind_vars
 
+        from petram.phys.em3d.dispersion_cold import build_coefficients
+        coeff1, coeff2, coeff3, coeff4 = build_coefficients(ind_vars, omega, B, dens_e, t_e,
+                                                            dens_i, masses, charges,
+                                                            self._global_ns, self._local_ns,)
+        '''
         Da = 1.66053906660e-27      # atomic mass unit (u or Dalton) (kg)        
         masses = np.array(masses, dtype=np.float64) * Da
         charges = np.array(charges, dtype=np.int64)
@@ -118,9 +128,6 @@ class EM1D_ColdPlasma(EM1D_Vac):
         
         def sigma(ptx):
             return - 1j*omega * np.zeros((3,3), dtype=np.complex128)
-            
-
-        from petram.phys.em3d.dispersion_cold import epsilonr_pl_cold, epsilonr_pl_cold_std
 
         params = {'omega': omega, 'masses': masses, 'charges': charges, }
 #                  'epsilonr_pl_cold': epsilonr_pl_cold,}
@@ -148,26 +155,25 @@ class EM1D_ColdPlasma(EM1D_Vac):
             coeff4 = NumbaCoefficient(mfem_coeff4)
             return coeff1, coeff2, coeff3, coeff4
         else:
-            return coeff1, coeff2, coeff3, ky, kz
+        '''
+        return coeff1, coeff2, coeff3, coeff4, ky, kz
 
     def add_bf_contribution(self, engine, a, real=True, kfes=0):
-        if kfes != 0:
-            return
         if real:
             dprint1("Add BF contribution(real)" + str(self._sel_index))
         else:
             dprint1("Add BF contribution(imag)" + str(self._sel_index))
 
-        coeff1, coeff2, coeff3, ky, kz = self.get_coeffs()
+        coeff1, coeff2, coeff3, coeff4, ky, kz = self.jited_coeff
         self.set_integrator_realimag_mode(real)
 
-        #if self.has_pml():
+        # if self.has_pml():
         #    coeff1 = self.make_PML_coeff(coeff1)
         #    coeff2 = self.make_PML_coeff(coeff2)
         #    coeff3 = self.make_PML_coeff(coeff3)
 
         ec = coeff1[kfes, kfes]
-        sc = coeff3[kfes, kfes]        
+        sc = coeff3[kfes, kfes]
 
         self.add_integrator(engine, 'epsilonr', ec, a.AddDomainIntegrator,
                             mfem.MassIntegrator)
@@ -178,119 +184,117 @@ class EM1D_ColdPlasma(EM1D_Vac):
         #
         #
         #
-        if kfes == 0: # Ex
+        if kfes == 0:  # Ex
             imu = coeff4*(ky**2 + kz**2)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #                self._local_ns, self._global_ns,
             #                real = real, factor = ky**2 + kz**2)
-            print("imu",imu)
+            print("imu", imu)
             self.add_integrator(engine, 'mur', imu, a.AddDomainIntegrator,
                                 mfem.MassIntegrator)
-          
-        elif kfes == 1 or kfes == 2: # Ey and Ez
+
+        elif kfes == 1 or kfes == 2:  # Ey and Ez
             imu = coeff4
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #                self._local_ns, self._global_ns,
             #                real = real)
             self.add_integrator(engine, 'mur', imu, a.AddDomainIntegrator,
                                 mfem.DiffusionIntegrator)
 
-            if kfes == 1: fac = kz*kz
-            if kfes == 2: fac = ky*ky
+            if kfes == 1:
+                fac = kz*kz
+            if kfes == 2:
+                fac = ky*ky
 
             imu = coeff4*fac
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #                self._local_ns, self._global_ns,
             #                real = real, factor = fac)
 
             self.add_integrator(engine, 'mur', imu, a.AddDomainIntegrator,
                                 mfem.MassIntegrator)
 
-        
-        #super(EM1D_ColdPlasma, self).add_bf_contribution(engine, a, real=real,
+        # super(EM1D_ColdPlasma, self).add_bf_contribution(engine, a, real=real,
         #                                                  kfes=kfes, ecsc=(eca,sc))
 
-    def add_mix_contribution(self, engine, mbf, r, c, is_trans, real = True):
+    def add_mix_contribution(self, engine, mbf, r, c, is_trans, real=True):
         if real:
-            dprint1("Add mixed contribution(real)" + "(" + str(r) + "," + str(c) +')'
-                    +str(self._sel_index))
+            dprint1("Add mixed contribution(real)" + "(" + str(r) + "," + str(c) + ')'
+                    + str(self._sel_index))
         else:
-            dprint1("Add mixed contribution(imag)" + "(" + str(r) + "," + str(c) +')'
-                    +str(self._sel_index))
-       
-        coeff1, coeff2, coeff3, ky, kz = self.get_coeffs()
+            dprint1("Add mixed contribution(imag)" + "(" + str(r) + "," + str(c) + ')'
+                    + str(self._sel_index))
+
+        coeff1, coeff2, coeff3, coeff4, ky, kz = self.jited_coeff
         self.set_integrator_realimag_mode(real)
 
-        #super(EM1D_ColdPlasma, self).add_mix_contribution(engine, mbf, r, c, is_trans,
+        # super(EM1D_ColdPlasma, self).add_mix_contribution(engine, mbf, r, c, is_trans,
         #                                                   real = real)
-        ec = coeff1[r, c]        
+        ec = coeff1[r, c]
         sc = coeff3[r, c]
 
         self.add_integrator(engine, 'epsilonr', ec, mbf.AddDomainIntegrator,
                             mfem.MixedScalarMassIntegrator)
         self.add_integrator(engine, 'sigma', sc, mbf.AddDomainIntegrator,
-                                mfem.MixedScalarMassIntegrator)
-        
+                            mfem.MixedScalarMassIntegrator)
+
         coeff4 = 1./coeff2[0, 0]
         if r == 0 and c == 1:
             imu = coeff4*(1j*ky)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=1j*ky)
             itg = mfem.MixedScalarDerivativeIntegrator
         elif r == 0 and c == 2:
-            imu = coeff4*(-1j*kz)            
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            imu = coeff4*(1j*kz)
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=1j*kz)
             itg = mfem.MixedScalarDerivativeIntegrator
         elif r == 1 and c == 0:
             imu = coeff4*(1j*ky)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=1j*ky)
             itg = mfem.MixedScalarWeakDerivativeIntegrator
         elif r == 1 and c == 2:
             imu = coeff4*(-ky*ky)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=-ky*kz)
             itg = mfem.MixedScalarMassIntegrator
         elif r == 2 and c == 0:
-            imu = coeff4*(-1j*kz)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            imu = coeff4*(1j*kz)
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=1j*kz)
             itg = mfem.MixedScalarWeakDerivativeIntegrator
         elif r == 2 and c == 1:
             imu = coeff4*(-ky*kz)
-            #imu = InvMu(m,  self.get_root_phys().ind_vars,
+            # imu = InvMu(m,  self.get_root_phys().ind_vars,
             #            self._local_ns, self._global_ns,
             #            real = real, factor=-ky*kz)
             itg = mfem.MixedScalarMassIntegrator
         else:
             assert False, "Something is wrong..if it comes here;D"
 
-           
         self.add_integrator(engine, 'mur', imu,
                             mbf.AddDomainIntegrator, itg)
-        
 
     def add_domain_variables(self, v, n, suffix, ind_vars, solr, soli=None):
         from petram.helper.variables import add_expression, add_constant
         from petram.helper.variables import (NativeCoefficientGenBase,
                                              NumbaCoefficientVariable)
 
-        
         if len(self._sel_index) == 0:
             return
-    
-        coeff1, coeff2, coeff3, coeff4 = self.get_coeffs(return_stix=True)
 
-        c1 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3,3))
-        c2 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3,3))
-        c3 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3,3))
-        c4 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3,3))        
+        coeff1, coeff2, coeff3, coeff4, ky, kz = self.get_coeffs()
+
+        c1 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3, 3))
+        c2 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3, 3))
+        c3 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3, 3))
+        c4 = NumbaCoefficientVariable(coeff1, complex=True, shape=(3, 3))
 
         ss = str(hash(self.fullname()))
         v["_e_"+ss] = c1
@@ -301,13 +305,14 @@ class EM1D_ColdPlasma(EM1D_Vac):
         self.do_add_matrix_expr(v, suffix, ind_vars, 'epsilonr', ["_e_"+ss])
         self.do_add_matrix_expr(v, suffix, ind_vars, 'mur', ["_m_"+ss])
         self.do_add_matrix_expr(v, suffix, ind_vars, 'sigma', ["_s_"+ss])
-        self.do_add_matrix_expr(v, suffix, ind_vars, 'Sstix', ["_spd_"+ss+"[0,0]"])
-        self.do_add_matrix_expr(v, suffix, ind_vars, 'Dstix', ["1j*_spd_"+ss+"[0,1]"])         
-        self.do_add_matrix_expr(v, suffix, ind_vars, 'Pstix', ["_spd_"+ss+"[2,2]"])        
-        
+        self.do_add_matrix_expr(v, suffix, ind_vars,
+                                'Sstix', ["_spd_"+ss+"[0,0]"])
+        self.do_add_matrix_expr(v, suffix, ind_vars, 'Dstix', [
+                                "1j*_spd_"+ss+"[0,1]"])
+        self.do_add_matrix_expr(v, suffix, ind_vars,
+                                'Pstix', ["_spd_"+ss+"[2,2]"])
+
         var = ['x', 'y', 'z']
         self.do_add_matrix_component_expr(v, suffix, ind_vars, var, 'epsilonr')
         self.do_add_matrix_component_expr(v, suffix, ind_vars, var, 'mur')
         self.do_add_matrix_component_expr(v, suffix, ind_vars, var, 'sigma')
-        
-
